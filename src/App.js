d819819c42e5785b6e6d684605999487405db6f8
RaiyanKhalil <raiyankhail@yahoo.com>
2020-08-26 18:00:46 +0600

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,35 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState } from 'react';
+//import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [number1, setNumber1] = useState(0);
+  const [total, setTotal] = useState(0);
+
+
+  function callSetTotal() {
+    setTotal(factorialize(number1));
+    function factorialize(number1) {
+      if (number1 < 0) return -1;
+      else if (number1 == 0) return 1;
+      else {
+        return number1 * factorialize(number1 - 1);
+      }
+    }
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>Factorial Calculator</h1>
+      <form className="number-inputs">
+        <input 
+        type="number" 
+        placeholder="Enter a number..."
+        value={number1}
+        onChange={(e) => setNumber1(+e.target.value)}
+        />
+      </form>
+      <button onClick={callSetTotal}>Calculate Factorial</button>
+      <h2>{total}</h2>
     </div>
   );
 }
